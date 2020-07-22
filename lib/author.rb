a1e72b4519f9dff04704eaab94aead476a00d428
John Kim <abcdefghijohnk@gmail.com>
2020-07-22 13:57:25 -0600

    first assertion in write books test pass

--- lib/author.rb
+++ lib/author.rb
@@ -10,4 +10,23 @@ class Author
   def name
     @first_name + " " + @last_name
   end
+
+  def write(title, publication_date)
+    book_info = {author_first_name: @author_first_name,
+                author_last_name: @author_last_name,
+                title: title,
+                publication_date: publication_date}
+
+    book = Book.new(book_info)
+    add_book(book)
+    return book
+  end
+
+  def add_book(book)
+    @books << book
+  end
+
+  def books
+    @books
+  end
 end
