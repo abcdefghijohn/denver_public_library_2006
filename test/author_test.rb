7583a535933beb92b4c02558fbfa9dacf94c3858
John Kim <abcdefghijohnk@gmail.com>
2020-07-22 13:17:54 -0600

    attribute test pass

--- test/author_test.rb
+++ test/author_test.rb
@@ -9,4 +9,11 @@ class AuthorTest < Minitest::Test
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     assert_instance_of Author, charlotte_bronte
   end
+
+  def test_it_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+    assert_equal [], charlotte_bronte.books
+  end
 end
