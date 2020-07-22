a98a1f544da5d10928590ebcd11e684cc5a1f64c
John Kim <abcdefghijohnk@gmail.com>
2020-07-22 14:06:16 -0600

    create library class and test pass

---
+++ test/library_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/author'
+require './lib/library'
+
+class LibraryTest < Minitest::Test
+
+  def test_it_exists
+    dpl = Library.new("Denver Public Library")
+    assert_instance_of Library, dpl 
+  end
+end
