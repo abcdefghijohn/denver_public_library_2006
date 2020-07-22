960b19d2fdd54d6d611dfac77c11eed9376a676c
John Kim <abcdefghijohnk@gmail.com>
2020-07-22 14:02:23 -0600

    write books test pass

--- test/author_test.rb
+++ test/author_test.rb
@@ -22,6 +22,10 @@ class AuthorTest < Minitest::Test
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
 
     assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
 
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villette], charlotte_bronte.books
   end
 end
